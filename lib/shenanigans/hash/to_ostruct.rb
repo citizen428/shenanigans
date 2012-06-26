ebaea0711f760e49ff6cf54e54a332f947c9f528
Michael Kohl <citizen428@gmail.com>
2012-06-26 21:27:59 +0200

    Fix documentation

--- lib/shenanigans/hash/to_ostruct.rb
+++ lib/shenanigans/hash/to_ostruct.rb
@@ -1,26 +1,25 @@
 require 'ostruct'
 
-# Recursively converts a <tt>Hash</tt> and all nested <tt>Hash</tt>es to
-# <tt>OpenStruct</tt>s. Especially useful for parsing YAML.
-#     yaml=<<EOY
-#     subject: Programming Languages
-#     languages:
-#       - name        : Ruby
-#         creator     : Matz
-#       - name        : Python
-#         creator     : Guido van Rossum
-#       - name        : Perl
-#         creator     : Larry Wall
-#     EOY
-#     struct = YAML.load(yaml).to_ostruct
-#     struct.subject
-#     #=> "Programming Languages"
-#     struct.languages.first
-#     #=> #<OpenStruct name="Ruby", creator="Matz">
-#     struct.languages.first.creator
-#     #=> "Matz"
-
 class Hash
+  # Recursively converts a <tt>Hash</tt> and all nested <tt>Hash</tt>es to
+  # <tt>OpenStruct</tt>s. Especially useful for parsing YAML.
+  #     yaml=<<EOY
+  #     subject: Programming Languages
+  #     languages:
+  #       - name        : Ruby
+  #         creator     : Matz
+  #       - name        : Python
+  #         creator     : Guido van Rossum
+  #       - name        : Perl
+  #         creator     : Larry Wall
+  #     EOY
+  #     struct = YAML.load(yaml).to_ostruct
+  #     struct.subject
+  #     #=> "Programming Languages"
+  #     struct.languages.first
+  #     #=> #<OpenStruct name="Ruby", creator="Matz">
+  #     struct.languages.first.creator
+  #     #=> "Matz"
   def to_ostruct
     arr = map do |k, v|
       case v
