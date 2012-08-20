106db02981a2177c4147b90599a5850972d65eed
Michael Kohl <citizen428@gmail.com>
2012-08-20 18:53:53 +0200

    New string method

---
+++ lib/shenanigans/string.rb
@@ -0,0 +1,1 @@
+require_relative "string/in_groups_of"
---
+++ test/string/test_in_groups_of.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'shenanigans/string/in_groups_of'
+
+class InGroupsOf < MiniTest::Unit::TestCase
+  def test_in_groups_of
+    str = "aabbcc"
+    assert str.in_groups_of(2) == ['aa', 'bb', 'cc']
+    assert "".in_groups_of(2) == []
+    assert_raises(ArgumentError) { "".in_groups_of(0) }
+  end
+end
